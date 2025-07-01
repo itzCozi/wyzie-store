cff7673c5e11e6eafbe9c1f7d1f30fdc5e7c51af
Cooper <Cooperransom08@gmail.com>
2025-07-01 20:34:00 +0000

    update store

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,34 @@ module.exports = {
     extend: {
       colors: {
         type: {
-          primary: "#d0d0d0",
-          emphasized: "#e0e0e0",
-          dimmed: "#757575",
+          primary: "#484848",
+          emphasized: "#303030",
+          dimmed: "#5c5c5c",
           dark: "#0a0a0a",
+        },
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1545b9",
+        },
+        border: {
+          50: "#f9fafb",
+          100: "#f3f4f6",
+          200: "#e5e7eb",
+          300: "#d1d5db",
+        },
+        success: {
+          400: "#34d399",
+          500: "#10b981",
+        },
+        error: {
+          500: "#ef4444",
         },
       },
     },
